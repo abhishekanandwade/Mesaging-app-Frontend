e4d8c9b34407b31646938e1916dbb9a5ec1c2996
Abhishek <abhi.anandwade@gmail.com>
2024-04-04 10:45:08 +0530

    Refactor Chat component styles

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,6 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile,MoreVert, SearchOutlined} from '@material-ui/icons'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import SendIcon from '@material-ui/icons/Send';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
 import React, { useState } from 'react'
@@ -10,7 +11,7 @@ import './Chat.css'
 
 function Chat({ messages }) {
 
-  const [input, setInput] = useState ('');
+  const [input, setInput] = useState('');
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -27,39 +28,39 @@ function Chat({ messages }) {
 
   };
   return (
-      <div className='chat'>
-        <div className="chat__header">
-          <Avatar className='avatar2'/>
-        
-          
-          <div className="chat__headerInfo">
-            <h3>Room name</h3>
-            <p>Online</p>
-          </div>
+    <div className='chat'>
+      <div className="chat__header">
+        <Avatar className='avatar2' />
 
-          <div className="chat__headerRight">
-            <IconButton>
-              <SearchOutlined style={{ color: "#afbac1" }}/>
-            </IconButton>
-            
-            <IconButton>
-              <MoreVert style={{ color: "#afbac1" }}/>
-            </IconButton>
-          </div>
-          </div>
 
-          <div className="chat__body">
-            {messages.map(message => (
-            <p className={`chat__message ${message.received && "chat__receiver"}`}> 
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Online</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined style={{ color: "#afbac1" }} />
+          </IconButton>
+
+          <IconButton>
+            <MoreVert style={{ color: "#afbac1" }} />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        {messages.map(message => (
+          <p className={`chat__message ${message.received && "chat__receiver"}`}>
             <span className='chat__name'>{message.name}</span>
             {message.message}
             <span className='chat__timestamp'>
               {message.timestamp}
             </span>
-            </p>
-            ))}
+          </p>
+        ))}
 
-            {/* <p className='chat__message chat__receiver'> 
+        {/* <p className='chat__message chat__receiver'> 
             <span className='chat__name'>Abhishek</span>
             This is a message
             <span className='chat__timestamp'>
@@ -67,29 +68,29 @@ function Chat({ messages }) {
             </span>
             </p>                     */}
 
-          </div>
-
-          <div className="chat__footer">
-            <IconButton>
-            <InsertEmoticonIcon style={{ color: "#afbac1" }} />
-            </IconButton>
-
-            <IconButton>
-            <AttachFile style={{ color: "#afbac1" }} />
-            </IconButton>
-
-            <form>
-              <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text"/>
-              <button onClick={sendMessage} type="submit">Send a message</button>
-            </form>
-            
-            <IconButton>
-            <MicIcon style={{ color: "#afbac1" }}/>
-            </IconButton>
-
-
-          </div>
       </div>
+
+      <div className="chat__footer">
+        <IconButton>
+          <InsertEmoticonIcon style={{ color: "#afbac1" }} />
+        </IconButton>
+
+        <IconButton>
+          <AttachFile style={{ color: "#afbac1" }} />
+        </IconButton>
+
+        <form>
+          <input value={input} onChange={e => { setInput(e.target.value) }} placeholder="Type a message" type="text" />
+          <button onClick={sendMessage} type="submit">Send a message</button>
+        </form>
+
+        <IconButton>
+          {input === '' ? <MicIcon style={{ color: "#afbac1" }} /> : <SendIcon style={{ color: "#afbac1"}}/>}
+        </IconButton>
+
+
+      </div>
+    </div>
   )
 }
 
